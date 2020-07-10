a85749af2a6bb8dba814a5f11831b4ceb51a7ae6
Tung Nguyen Thanh <tungnt2@officevnpay.com>
2020-07-10 18:58:48 +0700

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,19 @@
 const gulp = require('gulp');
+const cache = require('gulp-cache');
 const { series, parallel } = require('gulp');
 const sass = require('gulp-sass');
-const browserSync = require('browser-sync');
-const broswerSync = require('browser-sync').create();
+const browserSync = require('browser-sync').create();
 const sourcemaps = require('gulp-sourcemaps');
 const changed = require('gulp-changed');
-var replace = require('gulp-replace');
+const replace = require('gulp-replace');
+const dircompare = require('dir-compare');
+
+var imagemin = require('gulp-imagemin');
+var imageminPngquant = require('imagemin-pngquant');
+var imageminZopfli = require('imagemin-zopfli');
+var imageminMozjpeg = require('imagemin-mozjpeg'); //need to run 'brew install libpng'
+
+var del = require('del');
 var imgSrc = [];
 var imgDes = 'dist';
 
@@ -22,19 +30,52 @@ function style () {
     .pipe(browserSync.stream());
 }
 
-function findImages () {
-  return gulp.src('./src/*.html')
-    .pipe(replace(/([^('"]+)\.(jpg|png|jpeg|gif|svg|pdf|mp4)/g, match => {
-      var src = 'src/**/' + match.match(/([^\\\/:*?\"<>|]+)$/g);
-      imgSrc.indexOf(src) === -1 ? imgSrc.push(src) : '';
+// function findImages () {
+//   return gulp.src('./src/*.{html,css,js}')
+//     .pipe(replace(/([^('"]+)\.(jpg|png|jpeg|gif|svg|pdf|mp4)/g, match => {
+//       var src = 'src/**/' + match.match(/([^\\\/:*?\"<>|]+)$/g);
+//       imgSrc.indexOf(src) === -1 ? imgSrc.push(src) : '';
+//     })
+//     )
+// }
+function cleanMedia () {
+  return dircompare.compare('./src/media/', './dist/media/')
+    .then(res => res.diffSet.forEach(dif => {
+      dif.type1 == 'missing' ? del(dif.path2.replace(/(\\)/, '') + '/' + dif.name2) : ''
     })
     )
 }
 
-function images () {
-  return gulp.src(imgSrc, { allowEmpty: true })
-    .pipe(changed(imgDes))
-    .pipe(gulp.dest(imgDes))
+function media () {
+  return gulp.src('./src/media/**/*', { allowEmpty: true })
+    .pipe(changed('./dist/media/**/*'))
+    .pipe(gulp.dest('dist/media/'))
+}
+
+function imageMinify () {
+  return gulp.src('./src/media/**/*.{gif,png,jpg,svg,jpeg}')
+    .pipe(cache(imagemin([
+      //png
+      imageminPngquant({
+        speed: 1,
+        quality: [0.95, 1] //lossy settings
+      }),
+      imageminZopfli({
+        more: true
+        // iterations: 50 // very slow but more effective
+      }),
+      //svg
+      imagemin.svgo({
+        plugins: [{
+          removeViewBox: false
+        }]
+      }),
+      //jpg very light lossy, use vs jpegtran
+      imageminMozjpeg({
+        quality: 75
+      })
+    ])))
+    .pipe(gulp.dest('dist/media/'))
 }
 
 function watch () {
@@ -43,12 +84,20 @@ function watch () {
       baseDir: './dist/'
     }
   });
-  gulp.watch('./dist/scss/**/*.scss', style)
-  gulp.watch('./dist/**/*.html').on('change', broswerSync.reload);
-  gulp.watch('./dist/js/**/*.js').on('change', broswerSync.reload);
+  gulp.watch('src/scss/**/*.scss', style)
+  gulp.watch('dist/**/*.html').on('change', browserSync.reload);
+  gulp.watch('dist/js/**/*.js').on('change', browserSync.reload);
+}
+
+function clearCache () {
+  return cache.clearAll()
 }
 
 exports.style = style;
 exports.watch = watch;
+exports.cleanMedia = cleanMedia;
+exports.media = media;
+exports.imageMinify = imageMinify;
+exports.clearCache = clearCache;
 
-exports.dev = series(parallel(series(findImages, images), style), watch);
+exports.dev = series(parallel(series(cleanMedia, imageMinify), style), watch);
