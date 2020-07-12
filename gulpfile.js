d7bfe2dd174429d6fb81e734af81a8ff75311651
unknown <hihahihahoho@gmail.com>
2020-07-13 00:23:12 +0700

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require('gulp');
 const cache = require('gulp-cache');
+const cached = require('gulp-cached');
+const remember = require('gulp-remember');
 const { series, parallel } = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
@@ -7,15 +9,75 @@ const sourcemaps = require('gulp-sourcemaps');
 const changed = require('gulp-changed');
 const replace = require('gulp-replace');
 const dircompare = require('dir-compare');
+const image = require('gulp-image');
+const data = require('gulp-data');
+const path = require('path');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const newer = require('gulp-newer');
 
-var imagemin = require('gulp-imagemin');
-var imageminPngquant = require('imagemin-pngquant');
-var imageminZopfli = require('imagemin-zopfli');
-var imageminMozjpeg = require('imagemin-mozjpeg'); //need to run 'brew install libpng'
 
 var del = require('del');
 var imgSrc = [];
 var imgDes = 'dist';
+
+var pluginsBundlesJsVal = [];
+var pluginsBundlesCssVal = [];
+
+var pluginsDetail = {
+  'lightpick': {
+    "scripts": [
+      "src/plugins/bundles/lightpick/1moment.min.js",
+      "src/plugins/bundles/lightpick/lightpick.js",
+    ]
+  },
+  'bootstrap': {
+    "styles": [
+      "node_modules/bootstrap/dist/css/bootstrap.min.css"
+    ],
+    "scripts": [
+      "node_modules/bootstrap/js/dist/util.js",
+      "node_modules/bootstrap/js/dist/modal.js",
+      "node_modules/bootstrap/js/dist/dropdown.js",
+      "node_modules/bootstrap/js/dist/popover.js",
+      "node_modules/bootstrap/js/dist/tooltip.js",
+      "node_modules/bootstrap/js/dist/tab.js",
+    ]
+  }
+}
+
+var plugins = {
+  'bundles': [
+    pluginsDetail.bootstrap,
+    pluginsDetail.lightpick,
+  ]
+}
+
+plugins.bundles.forEach((item) => {
+  if (item.scripts != undefined) {
+    pluginsBundlesJsVal = pluginsBundlesJsVal.concat(item.scripts);
+  }
+  if (item.styles != undefined) {
+    pluginsBundlesCssVal = pluginsBundlesCssVal.concat(item.styles)
+
+  }
+});
+
+function pluginsBundlesJS () {
+  return gulp.src(pluginsBundlesJsVal)
+    .pipe(cached('pluginsBundlesJS'))
+    .pipe(remember('pluginsBundlesJS'))
+    .pipe(concat('bundles.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/plugins/bundles'));
+}
+
+function pluginsBundlesCss () {
+  return gulp.src(pluginsBundlesCssVal)
+    .pipe(concat('bundles.css'))
+    .pipe(gulp.dest('./dist/plugins/bundles'));
+}
+
 
 function style () {
   //1. where is my scss file
@@ -44,6 +106,7 @@ function cleanMedia () {
       dif.type1 == 'missing' ? del(dif.path2.replace(/(\\)/, '') + '/' + dif.name2) : ''
     })
     )
+    .catch(error => console.error(error));
 }
 
 function media () {
@@ -53,29 +116,26 @@ function media () {
 }
 
 function imageMinify () {
-  return gulp.src('./src/media/**/*.{gif,png,jpg,svg,jpeg}')
-    .pipe(cache(imagemin([
-      //png
-      imageminPngquant({
-        speed: 1,
-        quality: [0.95, 1] //lossy settings
-      }),
-      imageminZopfli({
-        more: true
-        // iterations: 50 // very slow but more effective
-      }),
-      //svg
-      imagemin.svgo({
-        plugins: [{
-          removeViewBox: false
-        }]
-      }),
-      //jpg very light lossy, use vs jpegtran
-      imageminMozjpeg({
-        quality: 75
-      })
-    ])))
+  return gulp.src('./src/media/**/*')
+    .pipe(cache(image({
+      pngquant: ['--quality=70-80', '--speed=1'],
+      optipng: false,
+      zopflipng: true,
+      jpegRecompress: false,
+      mozjpeg: ['-quality', 73],
+      gifsicle: true,
+      svgo: true,
+      concurrent: 10,
+      quiet: true // defaults to false
+    })))
     .pipe(gulp.dest('dist/media/'))
+}
+
+function dataTest () {
+  return gulp.src('src/**/*.html')
+    .pipe(cache(data(function (file) {
+      return console.log(path.relative(process.cwd(), file.path).replace(/([^\\]+)/g, '..').slice(0, -4));
+    })))
 }
 
 function watch () {
@@ -99,5 +159,7 @@ exports.cleanMedia = cleanMedia;
 exports.media = media;
 exports.imageMinify = imageMinify;
 exports.clearCache = clearCache;
+exports.dataTest = dataTest;
+exports.pluginsBundles = parallel(pluginsBundlesCss, pluginsBundlesJS);
 
 exports.dev = series(parallel(series(cleanMedia, imageMinify), style), watch);
