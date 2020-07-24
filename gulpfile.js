21d249941b5dcda326ab1d89bec8ecd1b52a6660
unknown <hihahihahoho@gmail.com>
2020-07-24 20:32:19 +0700

    h

--- gulpfile.js
+++ gulpfile.js
@@ -399,6 +399,7 @@ function pushFtp () {
   //   .pipe(f.restore)
   //   .pipe(conn.dest(process.env.FTP_PATH));
   return gulp.src(globs, { base: '.' })
+    .pipe(RevAll.revision())
     .pipe(conn.dest(process.env.FTP_PATH));
 }
 
