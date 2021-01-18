c93615a6d6750e17aa275a903264cf9d04c45540
tung <tungnt2@vnpay.vn>
2021-01-18 18:39:02 +0700

    update

---
+++ gulp_themes_config.js
@@ -0,0 +1,21 @@
+var themeColor = {
+  default: {
+    default: {
+      'grey-basic': '#afb3bc',
+      'grey-1': '#afb3bc',
+      'grey-2': '#e0e4eb',
+      'grey-3': '#eaeff3',
+      'grey-4': '#f7f8fa',
+      'base': '#121f3e',
+      'base-2': '#707c93',
+      'white': '#ffffff',
+      'red': '#ff3d4e',
+      'blue': '#3f3bec',
+      'orange': '#ff7544',
+      'yellow': '#e9ec3b',
+      'teal': '#0e5c22',
+      'green': '#2ed573',
+      'purple': '#4527a0'
+    }
+  }
+}
